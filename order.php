de8f8fab30c2ae604f760066c9773a3b805430fc
Torexto <89647038+Torexto@users.noreply.github.com>
2023-10-03 19:02:39 +0200

    Add files via upload

---
+++ order.php
@@ -0,0 +1,49 @@
+<?php
+$host = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "samochody";
+
+$connect = @new mysqli($host, $db_user, $db_password, $db_name);
+
+$nadwozie = $_POST["nadwozie"];
+$silnik = $_POST["silnik"];
+$wyp = $_POST["wyp"];
+$choinka = $_POST["choinka"];
+$imie = $_POST["imie"];
+$haslo = $_POST["haslo"];
+$kluczyk = $_POST["kluczyk"];
+$adres = $_POST["adres"];
+$telefon = $_POST["telefon"];
+$dzien = $_POST["dzien"];
+$czas = $_POST["czas"];
+$komentarz = $_POST["komentarz"];
+$kolor = $_POST["kolor"];
+
+$wyposazenie = "";
+$choinkatosql = "";
+
+for($i = 0; $i < sizeof($wyp) - 1; $i++)
+{
+    $wyposazenie = $wyposazenie.$wyp[$i].", ";
+}
+
+for($i = 0; $i < sizeof($choinka) - 1; $i++)
+{
+    $choinkatosql = $choinkatosql.$choinka[$i].", ";
+}
+
+$wyposazenie = $wyposazenie.$wyp[sizeof($wyp) - 1];
+$choinkatosql = $choinkatosql.$choinka[sizeof($choinka) - 1];
+
+// print("INSERT INTO `zamowienia`(`id`, `nadwozie`, `silnik`, `wyp`, `choinka`, `imie`, `haslo`, `kluczyk`, `adres`, `telefon`, `dzien`, `czas`, `komentarz`, `kolor`) 
+// VALUES (NULL,'$nadwozie','$silnik','$wyposazenie','$choinkatosql','$imie','$haslo','$kluczyk','$adres','$telefon','$dzien','$czas','$komentarz','$kolor')");
+
+if($query = @$connect->query(
+    sprintf("INSERT INTO `zamowienia`(`id`, `nadwozie`, `silnik`, `wyp`, `choinka`, `imie`, `haslo`, `kluczyk`, `adres`, `telefon`, `dzien`, `czas`, `komentarz`, `kolor`) 
+    VALUES (NULL,'$nadwozie','$silnik','$wyposazenie','$choinkatosql','$imie','$haslo','$kluczyk','$adres','$telefon','$dzien','$czas','$komentarz','$kolor')")))
+    {
+        header("Location: dziekujemy.html");
+    }
+
+$connect -> close();    
